95661d20bb911ed791261dc00e16e0bf834a1826
MarcelLeal <marcel@pgentulhos.com.br>
2025-10-23 19:41:20 -0300

    react person

--- src/components/Person/Person.jsx
+++ src/components/Person/Person.jsx
@@ -1,1 +1,25 @@
-// export const Person = ({ person }) => ();
+export const Person = ({ person }) => {
+  const ageBlock =
+    person.age != null ? (
+      <p className="Person__age">{`I am ${person.age}`}</p>
+    ) : null;
+
+  let partnerLabel;
+
+  if (!person.isMarried) {
+    partnerLabel = 'I am not married';
+  } else if (person.sex === 'm') {
+    partnerLabel = `${person.partnerName} is my wife`;
+  } else {
+    partnerLabel = `${person.partnerName} is my husband`;
+  }
+
+  return (
+    <section className="Person">
+      <h2 className="Person__name">{`My name is ${person.name}`}</h2>
+      {ageBlock}
+
+      <p className="Person__partner">{partnerLabel}</p>
+    </section>
+  );
+};
